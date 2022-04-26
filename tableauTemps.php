c024efba8f1d431a1de947025e73265df9d4443b
Eric Charbonnier <102246532+Eric514@users.noreply.github.com>
2022-04-26 13:35:35 -0400

    ajout couleur rouge sur variable XXXX et str_pad

--- tableauTemps.php
+++ tableauTemps.php
@@ -1,24 +1,22 @@
 <?php
-    // heure actuelle.
-$now = new DateTime("America/Toronto");
-echo $now->format('h:i'); 
-echo "<br>";
-$heuresJ = date("h") - 7;
+
+date_default_timezone_set('America/Toronto');
+$heuresJ = date("h");
 $minutesI = date("i");
-echo $heuresJ . " ";
-echo $minutesI;
+echo "Heure actuelle : <br>" . $heuresJ . " h " . $minutesI;
 echo "<br>";
+$color ="<span class=\"color\">XXXX</span>";
 $arr = array(); 
 for($j=0; $j<12; $j++){
     $arr[] = []; 
     for($i=0; $i<60; $i++){
-        $arr[$i][$j] = " " . ($j+1) . " h " . $i. " ";            
+        $arr[$i][$j] = ($j+1) . " h " . str_pad($i, 2, "0", STR_PAD_LEFT);          
     }            
 }
 for($j=0; $j<12; $j++){    
     for($i=0; $i<60; $i++){
-        if ($i == $minutesI && $j == $heuresJ)
-        $arr[$i][$j] = "XXXX";            
+        if ($i == $minutesI && $j == $heuresJ-1)
+        $arr[$i][$j] = $color;            
     }            
 }
 
@@ -29,29 +27,40 @@ for($j=0; $j<12; $j++){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page D'acceuil</title>
+    <title>Page Tableau</title>
     <style>
+        body {
+            background-color: #fde4cf;
+            text-align: center;
+        }
+        .color{
+            color : red;
+        }
         .tableStyle {
-        border-collapse: collapse;
-        background: rgb(245, 239, 223);
-        border: 1px solid ;
-        border-collapse: collapse;
-        margin: auto;
+            border: 5px solid #0096c7;
+            border-collapse: collapse;
+            margin: auto;
+        }
+        .tdtrStyle {
+            border: 2px solid #00b4d8;
+            border-collapse: collapse;
+            padding: 5px 5px;
         }
 
     </style>
 </head>
 <body>
     <h1>Travail Pratique 3</h1>
-    <?php echo '<table class=tableStyle>';
+    <a href="index.html">Page d'acceuil</a>
+    <?php echo '<table class="tableStyle">';
 for ($i = 0; $i < count($arr); $i++) {
-    echo '<tr class=tableStyle>';
+    echo '<tr class="tdtrStyle">';
     for ($j = 0; $j < count($arr[$i]); $j++) {
-        echo '<td class=tableStyle>' . $arr[$i][$j] . '</td>';
+        echo '<td class="tdtrStyle">' .  $arr[$i][$j] . "</td>";        
     }
-    echo '</tr>';
+    echo "</tr>";
 }
-echo '</table>';
+echo "</table>";
 ?>   
 </body>
 </html>
